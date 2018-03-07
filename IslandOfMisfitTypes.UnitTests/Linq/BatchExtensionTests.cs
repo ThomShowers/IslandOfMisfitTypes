2a56fc712517a415a6857ea45531acfc92683479
sl4gr00m <sl4gr00m@gmail.com>
2018-03-06 21:51:20 -0500

    Create Batch extension method for IEnumerable<T>.
    Creates an extension method that splits an IEnumerable<T> into an
    IEnumerable<IEnumerable<T>> using an IEnumerable<int> to specify the
    sizes.

---
+++ IslandOfMisfitTypes.UnitTests/Linq/BatchExtensionTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using IslandOfMisfitTypes.Linq;
+using Xunit;
+
+namespace IslandOfMisfitTypes.UnitTests.Linq
+{
+    public class BatchExtensionTests
+    {
+        [Fact]
+        public void Batch_NullSource_Throws()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(
+               () => (null as IEnumerable<int>).Batch(new[] { 1, 2, 3 }));
+            Assert.Equal("target", ex.ParamName);
+        }
+
+        [Fact]
+        public void Batch_NullBatchSizes_Throws()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => "abcdefg".Batch(null));
+            Assert.Equal("batchSizes", ex.ParamName);
+        }
+
+        [Fact]
+        public void Batch_EmptySource_ReturnsZeroBatches()
+        {
+            var batches = "".Batch(new[] { 1, 2, 3 });
+            Assert.Empty(batches);
+        }
+
+        [Fact]
+        public void Batch_EmptyBatchSizes_ReturnsZeroBatches()
+        {
+            var batches = "abcdefg".Batch(new int[0]);
+            Assert.Empty(batches);
+        }
+
+        [Fact]
+        public void Batch_SumOfBatchSizesIsLessThanSourceCount_BatchSizesAreCorrect()
+        {
+            var expected = new[] { 1, 2, 3 };
+            var actual = "abcdefg".Batch(expected).Select(b => b.Count());
+            Assert.True(expected.SequenceEqual(actual));
+        }
+
+        [Fact]
+        public void Batch_BatchSizesIncludesZeros_BatchSizesAreCorrect()
+        {
+            var expected = new[] { 1, 0, 2, 0, 3 };
+            var actual = "abcdefg".Batch(expected).Select(b => b.Count());
+            Assert.True(expected.SequenceEqual(actual));
+        }
+
+        [Fact]
+        public void Batch_BatchSizesIncludesNegatives_BatchSizesAreCorrect()
+        {
+            var batchSizes = new[] { 1, -1, 2, -2, 3 };
+            var expected = batchSizes.Select(s => s > 0 ? s : 0);
+            var actual = "abcdefg".Batch(expected).Select(b => b.Count());
+            Assert.True(expected.SequenceEqual(actual));
+        }
+
+        [Fact]
+        public void Batch_SumOfBatchSizesIsLessThanSourceCount_BatchContentsAreCorrect()
+        {
+            var batchSizes = new[] { 1, -1, 2, -2, 3 };
+            var expected = new[] { "a", "", "bc", "", "def" };
+            var actual =  "abcdefg".Batch(batchSizes);
+            Assert.True(expected.SequenceEqual(actual.Select(s => string.Concat(s))));
+        }
+
+        [Fact]
+        public void Batch_SumOfBatchSizesIsGreaterThanSourceCount_BatchSizesAreCorrect()
+        {
+            var batchSizes = new[] { 1, -1, 2, -2, 3, 7, 3 };
+            var expected = new[] { 1, 0, 2, 0, 3, 1 };
+            var actual = "abcdefg".Batch(batchSizes).Select(s => s.Count());
+            Assert.True(expected.SequenceEqual(actual));
+        }
+
+        [Fact]
+        public void Batch_SumOfBatchSizesIsGreaterThanSourceCount_BatchContentsAreCorrect()
+        {
+            var batchSizes = new[] { 1, -1, 2, -2, 3, 7, 3 };
+            var expected = new[] { "a", "","bc", "", "def", "g" };
+            var actual = "abcdefg".Batch(batchSizes);
+            Assert.True(expected.SequenceEqual(actual.Select(s => string.Concat(s))));
+        }
+    }
+}
---
+++ IslandOfMisfitTypes/Linq/BatchExtension.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IslandOfMisfitTypes.Linq
+{
+    /// <summary>
+    /// Adds 'Batch' extensions to <see cref="IEnumerable{T}"/> to split the collection into a
+    /// a series of enumerables each starting where the last one ended.
+    /// </summary>
+    public static class BatchExtension
+    {
+        /// <summary>
+        /// Enumerates <paramref name="target"/> as a series of <see cref="IEnumerable{T}"/>
+        /// intsances whose lengths are specified by <paramref name="batchSizes"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the element.</typeparam>
+        /// <param name="target">The <see cref="IEnumerable{T}"/> to batch.</param>
+        /// <param name="batchSizes">The sizes of each subsequent batch.</param>
+        /// <returns>The batches.</returns>
+        /// <remarks>
+        /// If the sum of <paramref name="batchSizes"/> is less than the Count() of
+        /// <paramref name="target"/> then the remaining elements will be skipped. If a batch size
+        /// is greater than or equal to the number of remaining elements the the next batch will
+        /// will be the final batch and include all remaining elements. Subsequent values in
+        /// <paramref name="batchSizes"/> will be ignored.
+        /// </remarks>
+        public static IEnumerable<IEnumerable<T>> Batch<T>(
+            this IEnumerable<T> target, IEnumerable<int> batchSizes)
+        {
+            if (target == null) throw new ArgumentNullException(nameof(target));
+            if (batchSizes == null) throw new ArgumentNullException(nameof(batchSizes));
+
+            // Move the actual IEnuerable implementation to another method so the parameter
+            // wont be deferred until evalutation.
+            return BatchImpl(target, batchSizes);
+        }
+
+        private static IEnumerable<IEnumerable<T>> BatchImpl<T>(
+            this IEnumerable<T> target, IEnumerable<int> batchSizes)
+        {
+            var source = target?.GetEnumerator();
+            foreach (var batchSize in batchSizes)
+            {
+                if (batchSize <= 0) yield return new T[0];
+                else if (source.MoveNext()) yield return GetBatch(source, batchSize);
+            }
+        }
+
+        private static IEnumerable<T> GetBatch<T>(IEnumerator<T> source, int count)
+        {
+            if (count <= 0) return new T[0];
+            var batch = new List<T>();
+            do { batch.Add(source.Current); } while (--count > 0 && source.MoveNext());
+            return batch;
+        }
+    }
+}
